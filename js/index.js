e0f5fef76753b3ce1fdd6e2c630e67348ca008ac
Choi Jung-Eun <64954700+CJE-cpu@users.noreply.github.com>
2026-05-29 17:17:06 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+let scroll = document.querySelectorAll(".scroll")
+let observer = new IntersectionObserver(function(entries){
+    entries.forEach(function(item){
+        if(item.isIntersecting){
+            item.target.classList.add("on")
+        }else{
+            item.target.classList.remove("on")
+        }
+    })
+}, {
+    threshold : 0.5
+})
+scroll.forEach(function(item){
+    observer.observe(item)
+})
+
+// window.addEventListener("scroll", function(){
+//     let lastScroll = 0
+//     if(window.scrollY > lastScroll){
+//         window.scrollBy(0, window.innerHeight)
+//     }
+// })
+
+
 let home5imgs = document.querySelectorAll(".home5img img")
 let cur = document.querySelector(".cur")
 
